50962cef252e6452559c2838e42f66a5526cf591
Rafael Vinicius <42884321+rafavini@users.noreply.github.com>
2025-02-11 07:14:21 -0300

    feature: Criação do portifolio parecido com o terminal do ubunto.

---
+++ data.js
@@ -0,0 +1,52 @@
+const data = {
+    commands: {
+        help: `
+            <li><code>help</code>: Mostrar informações sobre mim</li>
+            <li><code>clear</code>: Limpa o terminal</li>
+            <li><code>experiencias</code>: Mostra uma lista das minhas experiencias</li>
+            <li><code>sobreMim</code>: Mostra infomações sobre mim</li>
+        `,
+        sobreMim: `{<br>
+      &nbsp;&nbsp;&nbsp;"nome" : "Rafael Vinicius Germinari Gonçalves",<br>
+      &nbsp;&nbsp;&nbsp;"formação": "Ciência da computação - Universidade Federal de Mato Grosso do Sul",<br>
+      &nbsp;&nbsp;&nbsp;"linguagem" : "JS, HTML, CSS, C#, PHP",<br>
+      &nbsp;&nbsp;&nbsp;"framework" : "React, ASPNET",<br>
+      &nbsp;&nbsp;&nbsp;"IDE favorita": "VS Code",<br>
+      &nbsp;&nbsp;&nbsp;"cidade" : "Campo grande, Brasil"<br>
+    }`,
+        experiencias: `
+            <strong>Experiência Profissional:</strong>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Companhia</th>
+                        <th>Posição</th>
+                        <th>Descrição</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Jul 2021 - nov 2021 </td>
+                        <td>Ebserh</td>
+                        <td>Estágio</td=>
+                        <td>Desenvolvimento de uma wiki para o hospital universitário</td>
+                    </tr>
+                    <tr>
+                        <td>fev 2022 - nov 2023 </td>
+                        <td>EOS Systems</td>
+                        <td>Dev full stack</td=>
+                        <td>Desenvolvimento e gestão de soluções de segurança e autenticação, integradas à criação de APIs robustas e escaláveis. Implementação de sistemas para cálculo automatizado de contas de água, utilizando ASP.NET para desenvolvimento back-end e Crystal Reports para geração e impressão de relatórios personalizados.</td>
+                    </tr>
+                    <tr>
+                        <td>nov 2023 - Hoje</td>
+                        <td>SENAC</td>
+                        <td>Professor</td=>
+                        <td>Atuando em ensinar, gerenciar e acompanhar a fabrica de software em soluções para o mercado de trabalho </td>
+                    </tr>
+                </tbody>
+            </table> 
+        `,
+    },
+    defaultResponse: "Comando não reconhecido. Digite 'help' para ver a lista de comandos.",
+};
---
+++ functions.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const terminalBody = document.getElementById("terminal-body");
+    let inputField = document.getElementById("command-input");
+
+    function executeCommand(command) {
+        let output = document.createElement("div");
+        output.innerHTML = `<span class="prompt">rafael Vinicius:~/portifolio$</span> ${command}`;
+        terminalBody.insertBefore(output, inputField.parentElement);
+        
+        let response = document.createElement("div");
+        switch (command) {
+            case "help":
+                response.innerHTML = data.commands.help;
+                break;
+            case "clear":
+                terminalBody.innerHTML = "";
+                recreateInput();
+                return;
+            case "experiencias":
+                response.innerHTML = data.commands.experiencias;
+                break;
+            case "sobreMim":
+                response.innerHTML = data.commands.sobreMim;
+                break;
+            default:
+                response.textContent = data.defaultResponse;
+        }
+        terminalBody.insertBefore(response, inputField.parentElement);
+        terminalBody.scrollTop = terminalBody.scrollHeight;
+    }
+
+    function recreateInput() {
+        let inputLine = document.createElement("div");
+        inputLine.className = "input-line";
+        inputLine.innerHTML = '<span class="prompt">rafael Vinicius:~/portifolio$</span> <input type="text" id="command-input" autofocus>';
+        terminalBody.appendChild(inputLine);
+        inputField = document.getElementById("command-input");
+        inputField.focus(); // Foca automaticamente no novo campo de entrada
+        inputField.addEventListener("keypress", function (event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                executeCommand(inputField.value.trim());
+                inputField.value = "";
+            }
+        });
+    }
+
+    inputField.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            executeCommand(inputField.value.trim());
+            inputField.value = "";
+        }
+    });
+});
